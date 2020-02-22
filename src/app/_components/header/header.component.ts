c98fcebbd7ce3706f58d666202fb901a26e26845
Akif Batur <akifbatur@protonmail.com>
2020-02-22 19:32:40 +0300

    fix

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/_services/auth.service';
-import { ActivatedRoute } from '@angular/router';
-import * as jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +8,7 @@ import * as jwt_decode from 'jwt-decode';
 })
 export class HeaderComponent implements OnInit {
 
-  public username: string;
-
-  constructor(public authService: AuthService, private actRoute: ActivatedRoute) {
+  constructor(public authService: AuthService) {
     
   }
   
